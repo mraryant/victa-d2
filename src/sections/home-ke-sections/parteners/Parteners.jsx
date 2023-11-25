27195f20c7e7945c95d6a0dbb45188bca922d0f1
Aryant Soni <aryantshrirang4@gmail.com>
2023-11-25 16:51:36 +0530

    logo small button update

--- src/sections/home-ke-sections/parteners/Parteners.jsx
+++ src/sections/home-ke-sections/parteners/Parteners.jsx
@@ -3,10 +3,10 @@ import "./Parteners.css"
 // import 'slick-carousel/slick/slick.css';
 // import 'slick-carousel/slick/slick-theme.css';
 import partener1 from "../../../assets/logo/pr1.svg"
-import partener2 from "../../../assets/logo/pr2.svg"
-import partener3 from "../../../assets/logo/pr3.svg"
+import partener2 from "../../../assets/logo/pr2.png"
+import partener3 from "../../../assets/logo/pr3.png"
 import partener4 from "../../../assets/logo/pr4.svg"
-import partener5 from "../../../assets/logo/pr5.svg"
+// import partener5 from "../../../assets/logo/pr5.svg"
 
 const Parteners = () => {
   
@@ -20,7 +20,7 @@ const Parteners = () => {
         <img src={partener2} alt="" />
         <img src={partener3} alt="" />
         <img src={partener4} alt="" />
-        <img src={partener5} alt="" />
+        {/* <img src={partener5} alt="" /> */}
     </div>
     </div>
     </>
